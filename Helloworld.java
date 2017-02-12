fb4f338d5c73f97badd93eced09d5e5fa23764f2
CITEST007 <mobiletesterusf@gmail.com>
2017-02-11 22:21:30 -0600

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,8 @@ public class Helloworld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello World. This is my test java program");
+        System.out.println("Hello World. This is my test java program.");
+        System.out.println("Validating CI in Jenkins");
     }
 
 }
