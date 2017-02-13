b6979106f69f7813d65c03de20faefa0bbe634b0
CITEST007 <mobiletesterusf@gmail.com>
2017-02-12 19:07:49 -0600

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,6 @@ public class Helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World. This is my test java program.");
-        System.out.println("Hello World. This is my test java program to validate ci.");
     }
 
 }
